81955e1b7a827dbeace14cd857794b91c44d911b
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-04 22:48:06 +0300

    imported the random module

---
+++ details.py
@@ -0,0 +1,13 @@
+import random
+class Details:
+    """
+    Class that generates new instances of contacts
+    """
+    details_list = []
+
+    def __init__(self,username, password):
+
+     """
+    __init__ method that helps us define properties for our objects
+     """
+
