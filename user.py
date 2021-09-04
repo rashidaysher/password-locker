530ce6d4e361441efa4d14d292defec3b70663d1
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-04 20:41:40 +0300

    corrected indentation

--- user.py
+++ user.py
@@ -3,6 +3,7 @@ class User:
     Class that generates new instances of user.
     """
 user_list = []
+
 def __init__(self,username, password):
 
     """
@@ -12,4 +13,12 @@ def __init__(self,username, password):
      username: New user username
      password: New user password
      """
-   
+    self.username = username
+    self.password = password
+
+    def signup_user(self):
+
+         """
+         Function to signup users
+         """
+         User.user_list.append(self)
